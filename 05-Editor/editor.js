79eed64f484b3ea04d17379454d933327eec4a36
AngelPortilla1 <angelportihernan019@gmail.com>
2025-12-06 00:14:48 -0500

    Logica javascript añadida correctamente

---
+++ 05-Editor/editor.js
@@ -0,0 +1,21 @@
+
+
+
+const runbutton = document.getElementById('ejecutarBtn');
+const codeEditor = document.getElementById('textEditor');
+const output = document.getElementById('output');
+
+runbutton.addEventListener('click', runCode);
+
+
+function runCode(){
+    const code = codeEditor.value;
+
+    try{
+        const result = eval(code);
+        output.textContent = result;
+
+    } catch (error){
+        output.textContent = 'Error: ' + error.message;
+    }
+}
